0bed8f38efb31f93cacd3f042624f994f3a53931
jcmorai <carlosmorais.foto@globo.com>
2020-08-10 17:23:13 -0300

    alteracao da url

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
-const URL="https://java-api-spring-bot.herokuapp.com/validacpf?cpf="
+const URL="https://backend-api-turma6b-jcmorai.herokuapp.com/validacpf?cpf="
+
 function validar(){
 
 	var txtCPF = document.getElementById("txtCpf").value;
